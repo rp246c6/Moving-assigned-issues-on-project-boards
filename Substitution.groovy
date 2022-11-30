1e29b3e223e259c8eae3424f1f41748da16ac53f
rp246c6 <91663071+rp246c6@users.noreply.github.com>
2022-11-30 17:40:52 +0530

    Substitution file added

---
+++ Substitution.groovy
@@ -0,0 +1,5 @@
+// The confirmation variable is a string with substitution expressions
+def name = 'Steve'
+def numMessages = 2
+def confirmation = "${numMessages} message(s) sent to ${name}"
+println ("confirmation:"+confirmation)
