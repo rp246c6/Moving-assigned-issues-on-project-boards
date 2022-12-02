3c0689fe4d425f7b27ba57cd6ace57b963c39e1a
rp246c6 <91663071+rp246c6@users.noreply.github.com>
2022-12-02 12:45:54 +0530

    Added confirmation message to noremal strings

--- Substitution.groovy
+++ Substitution.groovy
@@ -1,5 +1,9 @@
 class Substitution {
  static void main(args){
+ // These are normal strings
+def name_1 = 'Steve'
+def confirmation_1 = '2 message(s) sent to ' + name_1
+println "confirmation:"+confirmation_1
 // The confirmation variable is a string with substitution expressions
 def name = 'Steve'
 def numMessages = 2
